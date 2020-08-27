a922efe9703119f3c252bfb875fbd12e76381986
Serkan Sahin <ssahin55@outlook.com>
2020-08-27 00:41:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,13 @@
-# string = "Hello there, and how are you?"
+# # string = "Hello there, and how are you?"
 
-def reverse_each_word(string)
-  words = string.split(" ")
-  reversed = []
-  words.each do |word|
-  reversed << word.reverse
-  end
-  reversed.join(" ")
-end
+# def reverse_each_word(string)
+#   words = string.split(" ")
+#   reversed = []
+#   words.each do |word|
+#   reversed << word.reverse
+#   end
+#   reversed.join(" ")
+# end
 
 
 
